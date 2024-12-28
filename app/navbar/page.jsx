7a9df1347eb9c16cd9b5638834ae7863f5a3dc95
Ali <ali.tradding021@gmail.com>
2024-12-28 15:23:07 +0330

    commit

--- app/navbar/page.jsx
+++ app/navbar/page.jsx
@@ -69,7 +69,7 @@ export default function Navbar() {
         ))}
       </ul>
       <button className="bg-sky-500 py-2 px-10 font-bold tex-xl border rounded-lg md:absolute md:left-10 text-white ">
-        <Link href="/login">ورود</Link>
+        <Link href="/login">ثبت نام</Link>
       </button>
     </nav>
   );
